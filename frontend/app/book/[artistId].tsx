6b719983633e1aa1bfaaee96431511f5cd9cc49c
annamabanlag-wq <annamabanlag@gmail.com>
2026-09-12 21:19:42 +0800

    Add Gcash QR payment details
    Added GCash payment instructions and details.

--- frontend/app/book/[artistId].tsx
+++ frontend/app/book/[artistId].tsx
@@ -478,6 +478,50 @@ if (paymentMethod === "gcash") {
   <View style={{ marginTop: 16 }}>
     <Text style={{ color: colors.text, fontWeight: "700", marginBottom: 8 }}>
       GCash Payment
+      <Image
+  source={require("../../assets/GCash-MyQR-1209202610418.PNG.jpg")}
+  style={{
+    width: 220,
+    height: 220,
+    alignSelf: "center",
+    marginTop: 12,
+    marginBottom: 12,
+  }}
+  resizeMode="contain"
+/>
+
+<Text
+  style={{
+    color: colors.text,
+    textAlign: "center",
+    fontWeight: "700",
+    fontSize: 16,
+    marginBottom: 4,
+  }}
+>
+  ROEBIN ENRIQUEZ
+</Text>
+
+<Text
+  style={{
+    color: colors.muted,
+    textAlign: "center",
+    marginBottom: 12,
+  }}
+>
+  GCash: 09381447214
+</Text>
+
+<Text
+  style={{
+    color: colors.text,
+    textAlign: "center",
+    fontWeight: "700",
+    marginBottom: 12,
+  }}
+>
+  Please send exactly P2,900 to the GCash account above.
+</Text>
     </Text>
 
     <Text style={{ color: colors.muted, marginBottom: 12 }}>
